a04ed1fa4f3d0ce055e0b39e54130e55fa0d4bed
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-18 08:56:09 +0530

    [akarsh] added: Perfect Number Program

---
+++ UC_2PerfectNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6assignment_LogicalPrograms
+{
+    class UC_2PerfectNumber
+    {
+        public void Checkperfectnumber()
+        {
+            int number; 
+            int sum = 0;
+            int n;
+          
+            Console.Write("enter the Number:");
+            number = int.Parse(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
