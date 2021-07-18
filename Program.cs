08e12dcc8d2d83af223f8ba7f6669aa282b317a7
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-18 08:30:50 +0530

    [akarsh] added: Welcome To Logical Programs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day6assignment_LogicalPrograms
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //UC_1FibonacciSeries fibonacciSeries = new UC_1FibonacciSeries();
+            //fibonacciSeries.Checkfibanocciseries();
+
+            //UC_2PerfectNumber perfectnumber = new UC_2PerfectNumber();
+            //perfectnumber.Checkperfectnumber();
+
+            //UC_3PrimeNumber primenumber = new UC_3PrimeNumber();
+            //primenumber.Checkprimenumber();
+
+            //UC_4Reverse_A_Number reverse_A_Number = new UC_4Reverse_A_Number();
+            //reverse_A_Number.reversenumber();
+
+            //UC_5CouponNumber couponNumber = new UC_5CouponNumber();
+            //couponNumber.Coupon();
+
+            //UC_6S stop = new UC_6S();
+            //stop.Stopwatch();
+
+
+
+
+        }
+    }
+}
