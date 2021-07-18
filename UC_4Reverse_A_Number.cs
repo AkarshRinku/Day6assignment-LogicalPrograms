cbcb3cda7faa57828e66287a0384c0c06153d6c1
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-18 09:00:16 +0530

    [akarsh] added: To Reverse A  Number Program.

---
+++ UC_4Reverse_A_Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6assignment_LogicalPrograms
+{
+    class UC_4Reverse_A_Number
+    {
+        public void reversenumber()
+        {
+            Console.WriteLine("Enter a No. to reverse");
+
+            int Number = int.Parse(Console.ReadLine());
+            int Reverse = 0;
+
+            while (Number > 0)
+            {
+                int remainder = Number % 10;
+                Reverse = (Reverse * 10) + remainder;
+                Number = Number / 10;
+            }
+            Console.WriteLine("Reverse No. is {0}", Reverse);
+            Console.ReadLine();
+        }
+    }
+}
